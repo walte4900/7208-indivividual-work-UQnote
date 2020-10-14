0eb35f0be07b19ce62ec5a5fd3faf81785d79669
Wang4900 <ziwen.wang1@uqconnect.edu.au>
2020-10-14 20:02:21 +0800

    Update create.php

--- UQNote/admin/create.php
+++ UQNote/admin/create.php
@@ -1,6 +1,6 @@
 <?php include("../path.php");?>
-<?php include(ROOT_PATH . "\app\controllers\users.php");?>
-<?php include(ROOT_PATH . "\app\controllers\post.php");?>
+<?php include(ROOT_PATH . "/app/controllers/users.php");?>
+<?php include(ROOT_PATH . "/app/controllers/post.php");?>
 
 <!DOCTYPE html>
 <html lang="en">
