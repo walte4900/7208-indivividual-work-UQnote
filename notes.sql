e8d93943f0db26afa6ce36bdfd1438c20eb0063b
Wang4900 <ziwen.wang1@uqconnect.edu.au>
2020-10-14 19:40:15 +0800

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2020 at 01:24 PM
+-- Generation Time: Oct 14, 2020 at 01:39 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -27,14 +27,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `posts`
 --
 
-CREATE TABLE `posts` \(
-  `id` int\(11\) NOT NULL,
-  `uname` varchar\(255\) NOT NULL,
-  `title` varchar\(255\) NOT NULL,
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `uname` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `subject` varchar\(255\) NOT NULL,
-  `date` datetime NOT NULL DEFAULT current_timestamp\(\)
-\) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `subject` varchar(255) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -42,13 +42,13 @@ CREATE TABLE `posts` \(
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` \(
-  `id` int\(11\) NOT NULL,
-  `name` varchar\(100\) NOT NULL,
-  `student_number` int\(8\) NOT NULL,
-  `password` int\(100\) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp\(\)
-\) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `student_number` int(8) NOT NULL,
+  `password` int(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -58,16 +58,16 @@ CREATE TABLE `users` \(
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY \(`id`\),
-  ADD KEY `uname` \(`uname`\);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `uname` (`uname`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY \(`id`\),
-  ADD UNIQUE KEY `OneStudentOneNote` \(`student_number`\),
-  ADD KEY `name` \(`name`\);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `OneStudentOneNote` (`student_number`),
+  ADD KEY `name` (`name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -77,13 +77,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int\(11\) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int\(11\) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -93,7 +93,7 @@ ALTER TABLE `users`
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY \(`name`\) REFERENCES `posts` \(`uname`\) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`name`) REFERENCES `posts` (`uname`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
