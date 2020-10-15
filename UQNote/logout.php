2acc79c70948d0a6de5fd2474a202155793fc9f3
Sven Wang <51254474+walte4900@users.noreply.github.com>
2020-10-16 03:40:25 +0800

    Update logout.php

--- UQNote/logout.php
+++ UQNote/logout.php
@@ -1,11 +1,9 @@
 <?php
 include('path.php');
 session_start();
-
-unset($_SESSION['id']);
-unset($_SESSION['name']);
-unset($_SESSION['msg']);
-unset($_SESSION['type']);
+session_destroy();
+setcookie(session_name() , "", time()-3600);
+$_SESSION = array();
 
 header('location:' . BASE_URL . 'index.php');
 exit();
